c7a123f3823bc2a417015ffc2b1fab4e0ef4dd45
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-16 11:54:41 +0300

    Initial Commit

---
+++ HW2.10.Metmuseum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.10.Metmuseum
+//
+//  Created by Dmitrii Onegin on 16.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
